b00b3e3c5a0d7b2ce89d68903fb3b0613ed7f538
Libasse Mbaye <79859400+Libassalaye24@users.noreply.github.com>
2026-04-23 13:28:02 +0000

    feat(money): implement Money value object with validation and addition logic

---
+++ src/main/java/com/e221/ecommerce/utils/AppConstants.java
@@ -0,0 +1,9 @@
+package com.e221.ecommerce.utils;
+
+import java.util.Set;
+
+public class AppConstants {
+    public AppConstants() {}
+
+    public static final Set<String> AUTHORIZED_CURRENCIES = Set.of("FCFA", "EUR");
+}
